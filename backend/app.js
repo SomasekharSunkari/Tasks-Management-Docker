e9a931d437ed6d8a09265053147e1bae561a1a53
Somasekhar <sunkarisekhar36@gmail.com>
2025-02-24 16:19:11 +0530

    Updated-20

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 const mongoUrl = process.env.MONGODB_URL;
+console.log(mongoUrl)
 mongoose.connect(mongoUrl, err => {
   if (err) throw err;
   console.log("Mongodb connected...");
@@ -26,7 +27,12 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
 }
 
+app.get("/", (req, res) => {
+  return res.json({ message: "Succes", status: 200 })
+
+})
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
 });
